5691ddfec1f23fd7917edabe502102538d26515a
Simone Bisogno <s.bisogno10@studenti.unisa.it>
2020-04-03 23:13:58 +0200

    Speed up cluster generation Significantly reduce execution time by setting most of remote commands in for loops as background processes but still monitoring their outcome

--- make_cluster.sh
+++ make_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PIDS=""
 TIME_POLLING=3
 SSH_ATTEMPTS=3 # 30 is way too much when multiplied for timeout
 NAME_FILE_PEM=*.pem
@@ -15,9 +16,22 @@ SPLASH_SCREEN="${SPLASH_SCREEN}\tSergio Guastaferro, 2018 [labgua]\n"
 SPLASH_SCREEN="${SPLASH_SCREEN}====================================================\n"
 
 ## custom defined functions
+checkBGProcesses() {
+	for p in $PIDS
+	do
+		if wait $p
+		then
+			echo > /dev/null
+		else
+			echo -e "\nAn error occurred for process $p"
+			echo "Failed phase: $PHASE"
+			exit
+		fi
+	done
+}
+
 checkError() {
 	local error_code=$1
-	local phase=$2
 
 	if (( $error_code != 0 ))
 	then
@@ -27,7 +41,7 @@ checkError() {
 		echo "Please check thoroughly CLI configuration or EC2 Security Group settings."
 		echo "A common solution would be creating a new Security Group."
 		echo
-		echo "Failed phase: $phase."
+		echo "Failed phase: $PHASE."
 		exit
 	fi
 }
@@ -112,7 +126,7 @@ aws ec2 run-instances --image-id $AMI --security-group-ids $SECURITY_GROUP \
 --count $DIM_CLUSTER --instance-type $INSTANCE_TYPE --key-name $KEY_NAME \
 --query 'Instances[*].InstanceId' \
 > data/id_instances.json
-checkError $? "$PHASE"
+checkError $?
 
 ## check whether aws command failed
 if [[ ! -s data/id_instances.json ]]; then
@@ -142,7 +156,7 @@ while [[ $ready_machines != $DIM_CLUSTER ]]; do
 	aws ec2 describe-instance-status --instance-ids $id_inst_params \
 	--query "InstanceStatuses[*].InstanceState.Name" \
 	> data/status_instances.json
-	checkError $? "$PHASE"
+	checkError $?
 
 	status_instances=( $(jq -r '.[]' data/status_instances.json) )
 
@@ -168,12 +182,12 @@ announcePhase "Getting instances public and private IPs"
 aws ec2 describe-instances --instance-ids $id_inst_params \
 --query 'Reservations[0].Instances[*].PublicIpAddress' \
 >> data/ip_list.json
-checkError $? "$PHASE"
+checkError $?
 
 aws ec2 describe-instances --instance-ids $id_inst_params \
 --query 'Reservations[0].Instances[*].PrivateIpAddress' \
 >> data/ip_private_list.json
-checkError $? "$PHASE"
+checkError $?
 echo "Saved on data/ip_list.json and data/ip_private_list.json"
 announcePhaseTermination
 
@@ -195,7 +209,7 @@ announcePhase "Checking SSH connections on instances"
 for pub_ip in "${ip_list[@]}"
 do
 	ssh -oStrictHostKeyChecking=no -oConnectionAttempts=$SSH_ATTEMPTS -i $LOCAL_PEM_AMAZON $USER_ACCESS@$pub_ip "exit;"
-	checkError $? "$PHASE"
+	checkError $?
 	echo "$pub_ip is READY!"
 done
 announcePhaseTermination
@@ -215,7 +229,7 @@ sudo -u $CUSER cp /home/$CUSER/.ssh/id_rsa.pub /home/$CUSER/.ssh/authorized_keys
 sudo -u $CUSER chmod -R 700 /home/$CUSER/.ssh;"
 
 ssh -oStrictHostKeyChecking=no -i $LOCAL_PEM_AMAZON $USER_ACCESS@$MASTER "$master_conf"
-checkError $? "$PHASE"
+checkError $?
 announcePhaseTermination
 
 announcePhase "Configuring SLAVES"
@@ -229,55 +243,67 @@ do
 	sudo chown $CUSER:root ../$CUSER/.ssh; \
 	echo -e \"$CPASS\n$CPASS\n\" | sudo passwd $CUSER;"
 
-	ssh -oStrictHostKeyChecking=no -i $LOCAL_PEM_AMAZON $USER_ACCESS@$curr_slave_ip "$slave_conf"
-	checkError $? "$PHASE"
+	ssh -oStrictHostKeyChecking=no -i $LOCAL_PEM_AMAZON $USER_ACCESS@$curr_slave_ip "$slave_conf" &
+	PIDS+=" $!"
+#	checkError $?
 done
+checkBGProcesses
 announcePhaseTermination
 
 announcePhase "Sending PEM on MASTER::$MASTER node"
 
 scp -i $LOCAL_PEM_AMAZON $LOCAL_PEM_AMAZON $USER_ACCESS@$MASTER:
-checkError $? "$PHASE"
+checkError $?
 announcePhaseTermination
 
 announcePhase "Sending ip_private_list on MASTER::$MASTER node in $CUSER space... [for future automation]"
 
 scp -i $LOCAL_PEM_AMAZON data/ip_private_list.array $USER_ACCESS@$MASTER:
-checkError $? "$PHASE"
+checkError $?
 ssh -oStrictHostKeyChecking=no -i $LOCAL_PEM_AMAZON $USER_ACCESS@$MASTER "sudo cp ip_private_list.array /home/$CUSER/"
-checkError $? "$PHASE"
+checkError $?
 announcePhaseTermination
 
 announcePhase "Setting names in /etc/hosts for all nodes"
 
-set_hosts="printf \"\n#AWS Build Cluster Script -- ip private nodes\n\" | sudo tee -a /etc/hosts; "
-set_hosts=$set_hosts"printf \"${ip_private_list[0]}   MASTER\n\" | sudo tee -a /etc/hosts; "
+set_hosts="printf \"\n# AWS Build Cluster Script -- nodes private IPs\n\" >> /etc/hosts; "
+set_hosts=$set_hosts"printf \"${ip_private_list[0]}   MASTER\n\" >> /etc/hosts; "
 for (( i=1; i<$DIM_CLUSTER; i++ ))
 do
-	set_hosts=$set_hosts"printf \"${ip_private_list[$i]}   NODE_$i\n\" | sudo tee -a /etc/hosts; "
+	set_hosts=$set_hosts"printf \"${ip_private_list[$i]}   NODE_$i\n\" >> /etc/hosts; "
 done
 
+PIDS=""
 for node in "${ip_list[@]}"
 do
-	ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$node "$set_hosts"
-	checkError $? "$PHASE"
+	ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$node "$set_hosts" &
+	PIDS=" $!"
+#	checkError $?
 done
+checkBGProcesses
+
+# print /etc/hosts from MASTER instead of printing all the ones from every node
+ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$MASTER "cat /etc/hosts;"
 announcePhaseTermination
 
 announcePhase "Sending id_rsa and id_rsa.pub from MASTER to SLAVES"
 
+PIDS=""
 for (( i=1; i<$DIM_CLUSTER; i++ ))
 do
 	curr_private_slave_ip=${ip_private_list[$i]}
 	send_rsa="sudo scp -oStrictHostKeyChecking=no -i $NAME_FILE_PEM ../$CUSER/.ssh/id_rsa ../$CUSER/.ssh/id_rsa.pub $USER_ACCESS@$curr_private_slave_ip:"
 
-	ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$MASTER "$send_rsa"
-	checkError $? "$PHASE"
+	ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$MASTER "$send_rsa" &
+	PIDS+=" $!"
+#	checkError $?
 done
+checkBGProcesses
 announcePhaseTermination
 
 announcePhase "Moving and setting permission for PEM files on slaves"
 
+PIDS=""
 for (( i=1; i<$DIM_CLUSTER; i++ ))
 do
 	curr_slave_ip=${ip_list[$i]}
@@ -289,9 +315,11 @@ do
 	sudo chown $CUSER:root /home/$CUSER/.ssh/authorized_keys; \
 	sudo chmod -R 700 /home/$CUSER/.ssh;"
 
-	ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$curr_slave_ip "$slave_pem_conf"
-	checkError $? "$PHASE"
+	ssh -i $LOCAL_PEM_AMAZON $USER_ACCESS@$curr_slave_ip "$slave_pem_conf" &
+	PIDS+=" $!"
+#	checkError $?
 done
+checkBGProcesses
 announcePhaseTermination
 
 echo ">>> Cluster is READY!"
